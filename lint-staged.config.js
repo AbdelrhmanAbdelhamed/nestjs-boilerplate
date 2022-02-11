412928cf3bbaed896ec934671926acc65131fae7
Abdelrahman Abdelhamed <abdelrahman.abdelhamed@jumia.com>
2022-02-11 20:00:41 +0200

    remove --bail test from pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  '*.{ts,js}': ['npm run lint', 'npm run test -- --bail --findRelatedTests'],
+  '*.{ts,js}': ['npm run lint', 'npm run test -- --findRelatedTests'],
   '*.md': ['npm run markdownlint'],
 };

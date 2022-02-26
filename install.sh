9863f8746a2243371f8cdb65e73f5e530ad90290
Abdelrahman Abdelhamed <abdelrahman.abdelhamed@jumia.com>
2022-02-27 01:55:11 +0200

    use internal docker network

--- install.sh
+++ install.sh
@@ -3,8 +3,18 @@ mkdir -p ./gateway/certs
 cd ./gateway/certs
 mkcert shortening.local.com "*.shortening.local.com"
 
-if ! grep -q shortening.local.com /etc/hosts; then
-    sudo sed -i '/^127\.0\.0\.1\s/s/$/ '"shortening.local.com"'/' /etc/hosts
-fi
+#TODO: Load and update hosts dynamically from docker-compose file
+declare -a hosts=("182.39.0.53 shortening.local.com" "182.39.0.52 shortening.database.local.com" "182.39.0.203 shortening.cache.local.com" "182.39.0.172 shortening.api.local.com")
+
+# get number of hosts
+numberOfHosts=${#hosts[@]}
+
+# use for loop to read all values and indexes
+for (( i = 0; i < ${numberOfHosts}; i++ ));
+do
+  if ! grep -q "${hosts[$i]}" /etc/hosts; then
+    echo "${hosts[$i]}" | sudo tee -a /etc/hosts > /dev/null
+  fi
+done
 
 docker-compose up -d --build
